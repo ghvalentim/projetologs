42aad3bb1b3f5edb8aa44fc6c5d097897950af2f
ghvalentim <gabrielvalentimcarvalho@gmail.com>
2026-06-12 15:14:41 +0100

    12-06_8

--- dashboard-app/bootstrap.sh
+++ dashboard-app/bootstrap.sh
@@ -21,15 +21,15 @@ docker compose up -d --build
 
 echo -e "\n=== Instalando dependências (Composer) ==="
 # Usando o container em execução para rodar o composer
-docker compose exec laravel-panel composer install --no-interaction --prefer-dist --optimize-autoloader
+docker compose exec laravel-app composer install --no-interaction --prefer-dist --optimize-autoloader
 
 echo -e "\n=== Configurando o Laravel ==="
-docker compose exec laravel-panel php artisan key:generate
+docker compose exec laravel-app php artisan key:generate
 
 # Uma pequena pausa para garantir que o banco de dados já aceita conexões antes da migração
 echo "Aguardando inicialização do banco de dados..."
 sleep 3 
 
-docker compose exec laravel-panel php artisan migrate --force
+docker compose exec laravel-app php artisan migrate --force
 
 echo -e "\n🚀 Ambiente configurado e rodando com sucesso!"
